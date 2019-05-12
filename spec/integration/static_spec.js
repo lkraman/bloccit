83f1d47a03318860100df177b16c1fb696ff8220
lkraman <laurakraman01@gmail.com>
2019-05-12 16:57:39 -0400

    commit assignment

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,23 +1,39 @@
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/";
+const aboutUs = "http://localhost:3000/about";
 
 describe("routes : static", () => {
 
-//#1
+  //#1
   describe("GET /", () => {
 
-//#2
+    //#2
     it("should return status code 200", (done) => {
 
-//#3
+      //#3
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
 
-//#4
+        //#4
         done();
       });
     });
 
+  });
+
+  describe("GET /about", () => {
+
+ //#2
+      it("should return status code 200 and have '/About Us/' in the body of the response", () => {
+
+ //#3
+       request.get(base, (err, res, body) => {
+         expect(res.statusCode).toBe(200);
+         expect(body).toContain("About Us");
+ //#4
+         done();
+      });
+    });
   });
 });
--- src/routes/static.js
+++ src/routes/static.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  res.send("Welcome to Bloccit");
-});
+const staticController = require("../controllers/staticController");
+router.get("/", staticController.index);
+
+router.get("/about", staticController.about);
 
 module.exports = router;
