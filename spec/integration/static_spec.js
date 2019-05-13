7426b7d85e92302c783b92153a10b5dc9d52e7c4
lkraman <laurakraman01@gmail.com>
2019-05-13 07:37:31 -0400

    fixing error on routing controllers view assignment

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,26 +1,23 @@
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/";
-const aboutUs = "http://localhost:3000/about";
+//const about = "http://localhost:3000/about";
 
 describe("routes : static", () => {
 
-  //#1
   describe("GET /", () => {
 
-    //#2
-    it("should return status code 200", (done) => {
+             it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", () => {
 
-      //#3
-      request.get(base, (err, res, body) => {
-        expect(res.statusCode).toBe(200);
+               request.get(base, (err, res, body) => {
+                 expect(res.statusCode).toBe(200);
+                 expect(body).toContain("Welcome to Bloccit");
 
-        //#4
-        done();
-      });
-    });
+                 done();
+               });
+              });
+            });
 
-  });
 
   describe("GET /about", () => {
 
