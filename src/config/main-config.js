500232e3e750b91bf90ef8aa8f9357a743fccb8b
lkraman <laurakraman01@gmail.com>
2019-05-31 13:38:30 -0400

    commit main-config

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -6,6 +6,7 @@ const expressValidator = require("express-validator");
 const session = require("express-session");
 const flash = require("express-flash");
 const passportConfig = require("./passport-config");
+const logger = require('morgan');
 
 module.exports = {
   init(app, express) {
@@ -31,5 +32,6 @@ module.exports = {
    })
     app.use(express.static(path.join(__dirname, '..', 'assets')));
 
+    app.use(logger('dev'));
   }
 };
